69912f352c77ab177a76d74021561c9b77b1e2e2
배소영 <sybae0@ecounterp.co.kr>
2023-05-22 21:37:21 +0900

    last commit

--- client/src/circleData.js
+++ client/src/circleData.js
@@ -1,11 +1,12 @@
 export function CircleData(items) {
     this.items = items;
+    
   }
   
   CircleData.prototype.getAll = function () {
     return this.items;
   };
-  
+    
   CircleData.prototype.getNext = function (item) {
     var index = this.items.indexOf(item);
     // index를 넘어가면 undefined가 나옴
